fd1c458247cbd8de467449f7c3a9982cc6b5ee6e
Nwoko-Repo <91076580+Nwoko-Repo@users.noreply.github.com>
2025-02-22 06:10:02 +0100

    init commit

---
+++ src/main/java/com/boltonuni/devop7303/Devop7303Application.java
@@ -0,0 +1,13 @@
+package com.boltonuni.devop7303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devop7303Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devop7303Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/boltonuni/devop7303/Devop7303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boltonuni.devop7303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devop7303ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
