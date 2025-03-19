b470250ddb43b12c8914ab61dce7be17635b3ad8
Nwoko-Repo <91076580+Nwoko-Repo@users.noreply.github.com>
2025-03-19 17:40:55 +0100

    registrations ok

--- src/main/java/com/boltonuni/devop7303/entity/User.java
+++ src/main/java/com/boltonuni/devop7303/entity/User.java
@@ -7,6 +7,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDateTime;
+
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
