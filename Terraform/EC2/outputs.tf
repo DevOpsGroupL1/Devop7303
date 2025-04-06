37b69fb7daf9a38b46886ed0277621649450eca5
hardarmyyy <adeyemoadams005@gmail.com>
2025-04-06 01:50:46 +0100

    CONFIG: update ec2 instance configuration file to provision application server

--- Terraform/EC2/outputs.tf
+++ Terraform/EC2/outputs.tf
@@ -1,6 +1,6 @@
 output "ec2_sg_id-public" {
   description = "The ID of the security group."
-  value       = aws_security_group.sg-public.id
+  value       = aws_security_group.sg-public[0].id
 }
 
 output "ec2-public_ip" {
