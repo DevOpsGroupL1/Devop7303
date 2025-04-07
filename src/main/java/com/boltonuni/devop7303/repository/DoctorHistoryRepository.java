aa8ea7e2792899d338c97e96aa83f334b357d5d6
Marius Vlad <marius_vlad10@yahoo.com>
2025-04-07 17:59:41 +0100

    Doctor History fixed

--- src/main/java/com/boltonuni/devop7303/repository/DoctorHistoryRepository.java
+++ src/main/java/com/boltonuni/devop7303/repository/DoctorHistoryRepository.java
@@ -4,13 +4,14 @@ import com.boltonuni.devop7303.entity.DoctorHistory;
 import com.boltonuni.devop7303.entity.Schedules;
 import com.boltonuni.devop7303.repository.DoctorHistoryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface DoctorHistoryRepository extends JpaRepository<Schedules, Integer> {
-    List<DoctorHistory> findByDoctorId;
-
-    List<DoctorHistory> findDoctorById(String doctorId);
+    @Query(value = "SELECT * from Schedules s where doctor_id = ?",nativeQuery = true)
+    List<Schedules> loadDoctorHistory(@Param("doctorId") String doctorId);
 }
 
